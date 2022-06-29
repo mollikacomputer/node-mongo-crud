6d1a4207fa4c693d0eb69590885f4841aa514e60
mollikacomputer <mollikacomputer3@gmail.com>
2022-06-29 06:09:04 +0600

    async function structure added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 
 app.use(cors());
@@ -21,6 +21,24 @@ const users = [
 app.get('/users', (req, res)=>{
     res.send(users);
 });
+
+// id	users  pass	    4p0QpTLyFR33djF0
+
+const uri = "mongodb+srv://users:4p0QpTLyFR33djF0@cluster0.kwdbbxh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// async function structure
+async function run(){
+
+}
+run().catch(console.dir);
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log('connect to db');
+//   client.close();
+// });
+
 // Query example
 app.get('/products/', async(req, res)=>{
     const {name, address} = req.query; // Object distuctaring শুধু অবজেক্ট পাওয়া যাবে
